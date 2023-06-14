f59ccb0b29226ba480f610c7c5aff572787ffead
Trilok Jain <trilok.jain@rubrik.com>
2023-06-14 10:12:44 +0530

    Scraping OTel metrics using OTel Prom and remote writing to Prom

--- samples/influx_udp.py
+++ samples/influx_udp.py
@@ -12,15 +12,17 @@ import socket
 
 
 def randomword(length):
-    letters = string.ascii_lowercase
+    # letters = string.ascii_lowercase
+    letters = ['0', 'b', 'c']
     return ''.join(random.choice(letters) for i in range(length))
 
 
 for idx in range(0, 10_000_000):
     # for idx in range(0, 10):
-    message = f"{randomword(16)}inf_tel_{randomword(6)},{randomword(5)}=server-{randomword(12)},{randomword(4)}=y{randomword(15)}jx,{randomword(4)}=y{randomword(15)}jx,{randomword(4)}=y{randomword(15)}jx,{randomword(4)}=y{randomword(15)}jx,{randomword(4)}=y{randomword(15)}jx,{randomword(4)}=y{randomword(15)}jx value={idx * 1000 / 7.0}"
+    message = f"a.b.c{randomword(16)}inf_tel_{randomword(6)},{randomword(5)}=server-{randomword(12)},{randomword(4)}=y{randomword(15)}jx,{randomword(4)}=y{randomword(15)}jx,{randomword(4)}=y{randomword(15)}jx,{randomword(4)}=y{randomword(15)}jx,{randomword(4)}=y{randomword(15)}jx,{randomword(4)}=y{randomword(15)}jx value={idx * 1000 / 7.0}"
+    message = f"some_thing,Hostess=0ssa{randomword(12)},other=y{randomword(15)}jx,{randomword(4)}=yy{randomword(15)}jx value={idx * 1000 / 7.0}"
     print(message)
-    time.sleep(0.01)
+    time.sleep(1)
     # time.sleep(1)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # Sends influx metrics on UDP port 8092
